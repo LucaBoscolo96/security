ecefa03db76d7a46e730016902ce5cabbbb8da05
l.boscolo.96@gmail.com <l.boscolo.96@gmail.com>
2022-07-19 13:08:50 +0200

    secure level 5 passport

--- app.js
+++ app.js
@@ -1,78 +1,109 @@
-const express =require('express')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const { log } = require('console')
-const mongoose= require('mongoose')
-mongoose.connect('mongodb://localhost:27017/userDB')
-const encrypt=require('mongoose-encryption')
-const bcrypt= require('bcrypt')
-const saltRounds=10
-const app=express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const { log } = require("console");
+const mongoose = require("mongoose");
+const app = express();
+const session = require("express-session");
+const passport = require("passport");
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect("mongodb://localhost:27017/userDB");
+const encrypt = require("mongoose-encryption");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+  email: String,
+  password: String,
 });
 
-const User = new mongoose.model('User', userSchema);
+userSchema.plugin(passportLocalMongoose);
 
-app.route('/')
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app
+  .route("/")
 
-.get(function(req,res){
-    res.render('home')
-})
+  .get(function (req, res) {
+    res.render("home");
+  });
 
-app.route('/login')
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-.get(function(req,res){
-    res.render('login')
-})
-.post(function(req,res){
-    const username = req.body.username;
-    const password= req.body.password;
-    User.findOne({email: username},function(err,foundUser){
-        if(!err){
-            if(foundUser){
-                console.log(foundUser.password);
-                console.log(password);
-                    bcrypt.compare(password, foundUser.password, function(err, result){
-                        if(result===true){
-                            res.render('secrets')
-                        }
-                })
-        }
-    }})
-})
+app
+  .route("/login")
 
-app.route('/register')
+  .get(function (req, res) {
+    res.render("login");
+  })
+  .post(function (req, res) {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.login(user, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    });
+  });
 
-.get(function(req,res){
-    res.render('register')
-})
-.post(function(req,res){
+app
+  .route("/register")
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-        email: req.body.username,
-        password: hash
-    })
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
+  .get(function (req, res) {
+    res.render("register");
+  })
+  .post(function (req, res) {
+    User.register(
+      { username: req.body.username },
+      req.body.password,
+      function (err, user) {
+        if (err) {
+          console.log(err);
+          res.redirect("/register");
         } else {
-            res.render('secrets')
+          passport.authenticate("local")(req, res, function () {
+            res.redirect("secrets");
+          });
         }
-    })
-})
+      }
+    );
+  });
 
-})
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
-
-
-app.listen(3000, function(){
-    console.log('server started on port 3000');
-})
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
