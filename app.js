563ec4a43d4dd8f22a6e18e8cf638ca244be2916
l.boscolo.96@gmail.com <l.boscolo.96@gmail.com>
2022-07-19 11:25:25 +0200

    secure level 3 md5

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { log } = require('console')
 const mongoose= require('mongoose')
 mongoose.connect('mongodb://localhost:27017/userDB')
 const encrypt=require('mongoose-encryption')
+const md5= require('md5')
 const app=express()
 
 console.log(process.env.SECRET);
@@ -39,7 +40,7 @@ app.route('/login')
 })
 .post(function(req,res){
     const username = req.body.username;
-    const password= req.body.password;
+    const password= md5(req.body.password);
     User.findOne({email: username},function(err,foundUser){
         if(!err){
             if(foundUser){
@@ -59,7 +60,7 @@ app.route('/register')
 .post(function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
